12ce08d257a45c6d3a18170bbe1d3ff69e6393b6
BekkerV8 <valkenshtein@gmail.com>
2022-06-01 14:55:57 -0700

    changed commit

--- src/test/java/ru/netology/javaQA6/services/SQRServiceTest.java
+++ src/test/java/ru/netology/javaQA6/services/SQRServiceTest.java
@@ -1,16 +1,22 @@
 package ru.netology.javaQA6.services;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.javaQA6.services.SQRService;
 
 public class SQRServiceTest {
     @ParameterizedTest
-    @CsvSource({
-            "10,90,6",
-            "9,500,20"
-    })
+    // @CsvSource({
+    //  "10,90,6",
+    //"9,500,20",
+    //  "0,5,2",
+    //  "-20,100,10"
+
+    // })
+    @CsvFileSource(files = "src/test/resources/range.of.numbers")
     public void shouldCalcExact(int from, int to, int expected) {
         SQRService service = new SQRService();
         //int expected = 6;
@@ -19,12 +25,13 @@ public class SQRServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    //@Test
-   // public void shouldCalcInexact() {
-     //   SQRService service = new SQRService();
-       // int expected = 20;
-       // int actual = service.calculateSQR(9, 500);
 
-       // Assertions.assertEquals(expected, actual);
-   // }
+    //@Test
+    // public void shouldCalcInexact() {
+    //   SQRService service = new SQRService();
+    // int expected = 20;
+    // int actual = service.calculateSQR(9, 500);
+
+    // Assertions.assertEquals(expected, actual);
+    // }
 }
