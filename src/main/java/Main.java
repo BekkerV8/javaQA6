38d0bf37675f2cb20d243924f1b459e0f9adf916
BekkerV8 <valkenshtein@gmail.com>
2022-06-02 03:43:39 -0700

    added a few tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,8 @@ import ru.netology.javaQA6.services.SQRService;
 
 public class Main {
     public static void main(String[] args) {
-        int limitLow = 95;
-        int limitUp = 600;
+        int limitLow = -225;
+        int limitUp = -10;
 
         SQRService service = new SQRService();
         System.out.println("Количество квадратов: " + service.calculateSQR(limitLow, limitUp));
